8173c122b4022c5f7347c3418464f0ff4af60b2c
Fabio Ferreira <fabio.ferreira@docway.co>
2025-09-30 22:47:14 -0300

    Config pre-commit

--- packages/react-lib/src/components/Button/Button.tsx
+++ packages/react-lib/src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps {
 }
 
 const Button = ({ some }: ButtonProps) => (
-  <div className={styles.container}>Button oh {some} </div>
+  <div className={styles.container}>Button {some} </div>
 );
 
 export default Button;
