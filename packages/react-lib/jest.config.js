719aa3be62d58865d97d3d415ec21c4985e96083
Fabio Ferreira <fabio.ferreira@docway.co>
2025-09-30 22:46:22 -0300

    Config pre-commit

--- packages/react-lib/jest.config.js
+++ packages/react-lib/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/.jestConfig/setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/.jestConfig/setup.js"],
   testMatch: ["**/src/components/**/*.test.tsx"],
   moduleNameMapper: {
     "\\.(css|scss)$": "identity-obj-proxy",
--- packages/react-lib/src/components/Button/Button.test.tsx
+++ packages/react-lib/src/components/Button/Button.test.tsx
@@ -5,5 +5,9 @@ import { render, screen } from "@testing-library/react";
 import Button from "./Button";
 
 describe("<Button />", () => {
-  it("should render component with default properties", () => {});
+  it("should render component with default properties", () => {
+    render(<Button some="Tag" />);
+    const element = screen.getByText("Button Tag");
+    expect(element).toBeInTheDocument();
+  });
 });
